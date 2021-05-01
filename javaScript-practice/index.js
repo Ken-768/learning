a6f1d6a8585978a8072ac9a506055dd7d60139f6
nakanokentarou <kcp.is.revolutionary@gmail.com>
2021-05-01 12:54:02 +0900

    question 9 & 11

--- javaScript-practice/index.js
+++ javaScript-practice/index.js
@@ -4,17 +4,17 @@ const b = {b : 'b'};
 const c = {...a, ...b};
 
 //問題2: const arry = ['aa','bb','cc','dd','ee','ff','gg']; のdd,ee,ffを新たな配列として返してください
-const arry = ['aa','bb','cc','dd','ee','ff','gg'];
-const newArry = arry.slice(-4, -1);
+const arry2 = ['aa','bb','cc','dd','ee','ff','gg'];
+const newArry = arry2.slice(-4, -1);
 // this might be helpful to understand slice method (https://www.javadrive.jp/javascript/string/index8.html)
 
 //問題3: ['a','b’] の要素をconsole出力してください e.g 'a'と'b'
-const arry = ['a', 'b'];
-arry.forEach(i => console.log(i));
+const arry3 = ['a', 'b'];
+arry3.forEach(i => console.log(i));
 
 //問題4: ['a', 'b']の各要素にindex値を足した文字列を出力してくださいe.g 'a0'と'b1'
-const arry = ['a', 'b'];
-arry.forEach(
+const arry4 = ['a', 'b'];
+arry4.forEach(
     function(el, i) {
         console.log(el + i);
     }
@@ -24,13 +24,38 @@ arry.forEach(
 Array.isArray([1, 2]);
 
 //問題8: keyとvalueを自身のプロパティのみ全て出力しなさい
-const obj = {
+const obj8 = {
     key: 'aa',
     key2: 'bb'
 }
 
-for(key in obj) {
-    if(obj.hasOwnProperty(key)){
-        console.log(key, obj[key]);
+for(key in obj8) {
+    if(obj8.hasOwnProperty(key)){
+        console.log(key, obj8[key]);
     }
 }
+
+//問題9:  ['a', 'b', 'c'] 配列の中の全ての要素を結合し、1つの文字列として出力してください。
+const array9 = ['a', 'b', 'c'];
+
+array9.join = "";
+//or
+let str = '';
+const count = array9.length;
+
+for (let i = 0; i < count; i++) {
+    str += array9[i];
+}
+
+str
+
+//問題11: id番号が若い順にソートしたオブジェクトを含む配列を出力してください
+let arry11 =[
+    {id:1,name:'morita'},
+    {id:2,name:'kenji'},
+    {id:4,name:'uro'},
+    {id:3,name:'ken'}
+];
+arry11.sort(function(a,b){
+    return a.id > b.id
+})
